a68740db1dee5d9afaa19693d1fb8064073199df
Ana <anaemilia.deorellana@gmail.com>
2023-07-28 20:17:06 -0300

    add titles

--- src/app/research/page.jsx
+++ src/app/research/page.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 const Page = () => {
   return (
     <div>
+      <h1>Research</h1>
       <p>Nothing to see here yet :(</p>
     </div>
   );
--- src/app/teaching/page.jsx
+++ src/app/teaching/page.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
-const teaching = () => {
+const Teaching = () => {
   return (
     <div>
+      <h1>Teaching</h1>
       <p>Nothing to see here yet :(</p>
     </div>
   );
 };
 
-export default teaching;
+export default Teaching;
