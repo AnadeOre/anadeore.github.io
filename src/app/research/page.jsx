00b6726a567fd66f7c54c1dd3220cad7abae69bf
Ana <anaemilia.deorellana@gmail.com>
2023-07-29 00:25:01 -0300

    add undergrad file

--- src/app/research/page.jsx
+++ src/app/research/page.jsx
@@ -1,10 +1,18 @@
+import Link from 'next/link';
 import React from 'react';
 
 const Page = () => {
   return (
     <div>
       <h1>Research</h1>
-      <p>Nothing to see here yet :(</p>
+      <ul>
+        <li>
+          <h4>Undergraduate dissertation</h4>
+          <Link href='https://anadeore.github.io/files/UndergradDissertation.pdf'>
+            Espacios de tipo homogéneo y metrización
+          </Link>
+        </li>
+      </ul>
     </div>
   );
 };
