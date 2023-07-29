e8b3341ab260b27f8d3a46a508e06188a0d7eeeb
Ana <anaemilia.deorellana@gmail.com>
2023-07-29 01:01:17 -0300

    add teaching

--- src/app/teaching/page.jsx
+++ src/app/teaching/page.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
+import styles from '../styles/list.module.css';
 
 const Teaching = () => {
   return (
     <div>
       <h1>Teaching</h1>
-      <p>Nothing to see here yet :(</p>
+      <ul>
+        {/*  */}
+        <li>
+          <h3>2023</h3>
+          <div className={styles.indentedSection}>
+            <p>Tutor for "Matemática A", 1° Semester.</p>
+            <p>Tutor for "Matemática Básica", 1° Semester.</p>
+            <p>Tutor for Engineering entrants, University Admissions Period.</p>
+          </div>
+        </li>
+        {/*  */}
+        <li>
+          <h3>2022</h3>
+          <div className={styles.indentedSection}>
+            <p>Tutor for "Cálculo 1", 2° Semester.</p>
+            <p>Tutor for "Matemática Discreta", 2° Semester.</p>
+          </div>
+        </li>
+      </ul>
     </div>
   );
 };
