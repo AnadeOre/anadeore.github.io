4a9ef7758948bd6ed166f1e761064971e2aa16fb
Ana <anaemilia.deorellana@gmail.com>
2023-07-29 01:01:27 -0300

    change styles name

--- src/app/components/DissertationDiv.jsx
+++ src/app/components/DissertationDiv.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import Link from 'next/link';
-import styles from '../styles/dissertation.module.css';
+import styles from '../styles/list.module.css';
 
 const DissertationDiv = ({title, description, linkURL, linkDesc}) => {
   return (
     <li>
-      <h4>{title}</h4>
+      <h3>{title}</h3>
       <div className={styles.indentedSection}>
         <p className={styles.description}>{description}</p>
         <Link className='linkDecor' target='_blank' href={linkURL}>
