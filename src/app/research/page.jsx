3e6045816883da5a05d5a3d9cb995001c8dd7c68
Ana <anaemilia.deorellana@gmail.com>
2023-12-10 10:13:48 +0000

    update research interests

--- src/app/research/page.jsx
+++ src/app/research/page.jsx
@@ -11,9 +11,10 @@ const Page = () => {
         Fourier dimension is as large as it can be, being equal to their
         Hausdorff dimension. In these situations, where we have two dimensions
         that share some characteristics, dimension interpolation is useful to
-        gain insight on their relation. The Fourier dimension spectrum is a
-        family of dimensions that live between the Fourier and the Hausdorff
-        dimensions for sets and Fourier and Sobolev dimensions for measures.
+        gain insight on their relation. I'm currently studying the Fourier
+        dimension spectrum, a family of dimensions that live between the Fourier
+        and the Hausdorff dimensions for sets and Fourier and Sobolev dimensions
+        for measures.
         {/* Unerstanding how these dimensions */}
         {/* behave gives more insight into the relation between the dimensions it */}
         {/* interpolates. */}
