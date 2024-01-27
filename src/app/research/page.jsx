d526e1b69816e3d0aa4268b0e319f3f003aa9e6f
Ana <anaemilia.deorellana@gmail.com>
2024-01-27 21:17:47 +0000

    update research

--- src/app/research/page.jsx
+++ src/app/research/page.jsx
@@ -5,16 +5,17 @@ const Page = () => {
     <div>
       <h1>Research</h1>
       <p>
-        The Fourier dimension of a set quentifies how structured vs how random
-        that set is. That is why the Cantor set (very structured) has Fourier
-        dimension zero, but random sets are usually Salem sets, i.e. their
-        Fourier dimension is as large as it can be, being equal to their
-        Hausdorff dimension. In these situations, where we have two dimensions
-        that share some characteristics, dimension interpolation is useful to
-        gain insight on their relation. I'm currently studying the Fourier
-        dimension spectrum, a family of dimensions that live between the Fourier
-        and the Hausdorff dimensions for sets and Fourier and Sobolev dimensions
-        for measures.
+        The Hausdorff dimension of a set gives information regarding its
+        geometric scaling properties. The Fourier dimension on the other hand
+        quentifies how structured vs how random that set is. That is why the
+        Cantor set (very structured) has Fourier dimension zero, but random sets
+        are usually Salem sets, i.e. their Fourier dimension is as large as it
+        can be, being equal to their Hausdorff dimension. In these situations,
+        where we have two dimensions that behave differently and capture
+        different information, dimension interpolation is useful to gain insight
+        on their relation. I'm currently studying the Fourier spectrum, a family
+        of dimensions that live between the Fourier and the Hausdorff dimensions
+        for sets and Fourier and Sobolev dimensions for measures.
         {/* Unerstanding how these dimensions */}
         {/* behave gives more insight into the relation between the dimensions it */}
         {/* interpolates. */}
@@ -22,7 +23,7 @@ const Page = () => {
       <ul>
         <DissertationDiv
           title='Undergraduate dissertation (spanish)'
-          description='For the title "Licenciada en matemática aplicada" at UNL. Supervised by
+          description='"Licenciatura" thesis at UNL. Supervised by
           Marisa Toschi and Mauricio Ramseyer.'
           linkURL='/UndergradDissertation.pdf'
           linkDesc='Homogeneous Type Spaces and Metrisation'
