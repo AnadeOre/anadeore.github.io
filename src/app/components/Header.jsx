76e24faee3255ba54f5f029a924d4822de8c19df
Ana <anaemilia.deorellana@gmail.com>
2024-03-20 15:45:30 +0000

    Oops, change font back

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -6,12 +6,6 @@ import styles from '../styles/header.module.css';
 const Header = () => {
   return (
     <header className={styles.header}>
-      <link rel='preconnect' href='https://fonts.googleapis.com' />
-      <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin />
-      <link
-        href='https://fonts.googleapis.com/css2?family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap'
-        rel='stylesheet'
-      />
       <div className={styles.headerContainer}>
         <h2 className={styles.h2}>
           <Link className={styles.title} href='/'>
