784c171c1f6375defbb261912e1c3593d4476dcd
Ana <anaemilia.deorellana@gmail.com>
2024-06-13 00:46:22 +0100

    add main page

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,96 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-
+import Image from 'next/image';
+import styles from './styles/page.module.css';
+import Footer from './components/Footer';
+import Link from 'next/link';
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <h1>About</h1>
+      <div className={styles.infoContainer}>
+        <Image
+          src='/Image.jpeg'
+          className={styles.profilePic}
+          width={200}
+          height={1140}
+          layout='intrinsic'
+          alt='Profile picture'
+        />
+        <div className={styles.description}>
+          <p>
+            I am currently a Maths PhD student at the University of St Andrews,
+            supervised by{' '}
+            <Link
+              className='linkDecor'
+              target='_blank'
+              href='https://jonathan-fraser.github.io/homepage/'>
+              Jonathan Fraser
+            </Link>{' '}
+            and{' '}
+            <Link
+              className='linkDecor'
+              target='_blank'
+              href='https://kennethfalconer.github.io'>
+              Kenneth Falconer
+            </Link>
+            . I completed my undergraduate studies in mathemathics at the
+            Universidad Nacional del Litoral, in Argentina.
+          </p>
+          <p>
+            My research interests are in dimension theory. Specifically, in the
+            connection between geometric measure theory and Fourier analysis
+            through the Fourier spectrum. Visit my{' '}
+            <Link href='/research' className='linkDecor'>
+              research page
+            </Link>{' '}
+            for more details.
+          </p>
+          <p>
+            I'm the creator behind the{' '}
+            <Link
+              href='https://www.youtube.com/problemathic'
+              target='_blank'
+              className='linkDecor'>
+              Problemathic YouTube channel
+            </Link>{' '}
+            and{' '}
+            <Link
+              href='https://www.instagram.com/problemathicvideos/'
+              target='_blank'
+              className='linkDecor'>
+              Instagram page
+            </Link>
+            . In addition to this, my hobbies include having small{' '}
+            <Link
+              href='https://animaths.vercel.app'
+              target='_blank'
+              className='linkDecor'>
+              programming projects
+            </Link>
+            , mostly in web development.
+          </p>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <br />
+      <address className={styles.address}>
+        <h3>Contact Details</h3>
+        <Link
+          className='linkDecor'
+          href='mailto:aedo1@st-andrews.ac.uk'
+          target='_blank'>
+          aedo1@st-andrews.ac.uk
+        </Link>
+        <br />
+        103, Mathematics Institute
+        <br />
+        University of St Andrews
+        <br />
+        North Haugh, St Andrews
+        <br />
+        KY16 9SS
+        <br />
+        Scotland, UK
+      </address>
+      <Footer />
     </main>
   );
 }
