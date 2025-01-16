1ad0c8ca7b0922e47507f605371fc2521cbc11ac
Ana <anaemilia.deorellana@gmail.com>
2025-01-16 11:18:10 +0000

    change HAPDE abstract style

--- src/app/HAPDE/ScheduleHAPDE.jsx
+++ src/app/HAPDE/ScheduleHAPDE.jsx
@@ -1,10 +1,15 @@
 'use client';
-import React from 'react';
+import React, {useState} from 'react';
 import Link from 'next/link';
 import styles from '../styles/list.module.css';
 import stylesHAPDE from './stylesHAPDE.module.css';
+import 'katex/dist/katex.min.css';
+import Latex from 'react-latex-next';
+
+import {SlArrowDown, SlArrowUp} from 'react-icons/sl';
 
 const Schedule = ({time, title, abstract, slides, speaker}) => {
+  const [viewAbstract, setViewAbstract] = useState(false);
   return (
     <li key={title}>
       <div>
@@ -13,6 +18,24 @@ const Schedule = ({time, title, abstract, slides, speaker}) => {
       </div>
       <span>{'   '}</span>
       <b>{title}</b>
+      <span>{'   '}</span>
+      {abstract !== undefined ? (
+        <>
+          <button
+            className={styles.absButtonPubli}
+            onClick={() => setViewAbstract((viewAbstract) => !viewAbstract)}>
+            {viewAbstract ? <SlArrowUp /> : <SlArrowDown />}
+          </button>
+          {''}
+          {viewAbstract && (
+            <div className={styles.abstractPubli}>
+              <Latex>{abstract}</Latex>
+            </div>
+          )}
+        </>
+      ) : (
+        ''
+      )}
       {/* (
       {slides && (
         <Link href={slides} target='_blank' className='linkDecor'>
@@ -21,7 +44,7 @@ const Schedule = ({time, title, abstract, slides, speaker}) => {
       )}
       ) */}
       <br />
-      {abstract && <div className={styles.abstractPubli}>{abstract}</div>}
+      {/* {abstract && <div className={styles.abstractPubli}>{abstract}</div>} */}
     </li>
   );
 };
