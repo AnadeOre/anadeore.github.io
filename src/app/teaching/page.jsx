f6f35fb6119483956529eaab9ec0e830977efb74
Ana E. de Orellana <anaemilia.deorellana@gmail.com>
2025-11-24 16:45:46 +0000

    update teaching page

--- src/app/teaching/page.jsx
+++ src/app/teaching/page.jsx
@@ -4,68 +4,69 @@ import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 const Teaching = () => {
+  const style = {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 1,
+  };
   return (
     <div>
       <Header />
       <h1>Teaching</h1>
       <ul>
         <li>
-          <h3>2025-2026: University of St Andrews</h3>
+          <h3>
+            Academic Tutor and Demonstrator:{' '}
+            <span style={style}>Sep. 2023 - Dec. 2025</span>
+          </h3>
           <div className={styles.indentedSection}>
             <p>
-              Tutor for MT2502: Analysis (5 tutorials + 3 examples classes),
-              Autumn.
-            </p>
-            <p>
-              Tutor for MT1001: Introductory Mathematics (2 tutorials), Autumn.
+              Tutor at the University of St Andrews for a variety of pure and
+              applied undergraduate mathematics modules. Marking summative and
+              formative assessment for over 150 students. Demonstrator for over
+              550 students in examples classes.
             </p>
           </div>
         </li>
         <li>
-          <h3>2024-2025: University of St Andrews</h3>
-          <div className={styles.indentedSection}>
-            <p>Tutor for MT1003: Mathematics (3 tutorials), Spring.</p>
-            <p>
-              Tutor for MT1002: Mathematics (3 tutorials + 2 examples classes),
-              Spring.
-            </p>
-            <p>
-              Tutor for MT2502: Analysis (3 tutorials + 2 examples classes),
-              Autumn.
-            </p>
-            <p>
-              Tutor for MT1001: Introductory Mathematics (3 tutorials), Autumn.
-            </p>
-          </div>
-        </li>
-        {/*  */}
-        <li>
-          <h3>2023-2024: University of St Andrews</h3>
+          <h3>
+            Lecturer: <span style={style}>Feb. 2023</span>
+          </h3>
+
           <div className={styles.indentedSection}>
             <p>
-              Tutor for MT1003: Pure and Applied Mathematics (2 tutorials + 2
-              examples classes), Spring.
-            </p>
-            <p>
-              Tutor for MT2502 Analysis: (2 tutorials + 2 examples classes),
-              Autumn.
+              Lecturer at the Universidad Nacional del Litoral's `Summer School'
+              for entrance level students studying core mathematics modules.
+              Lectured two groups of 70 students twice per week in two-hour
+              sessions.
             </p>
           </div>
         </li>
         <li>
-          <h3>2023: Universidad Nacional del Litoral</h3>
+          <h3>
+            Teaching Assistant <span style={style}>Aug. 2022 - Mar. 2023</span>
+          </h3>
           <div className={styles.indentedSection}>
-            <p>Tutor for "Matemática A", 1° Semester.</p>
-            <p>Tutor for "Matemática Básica", 1° Semester.</p>
-            <p>Tutor for Engineering entrants, University Admissions Period.</p>
+            <p>
+              Lecturer at the Universidad Nacional del Litoral's `Summer School'
+              for entrance level students studying core mathematics modules.
+              Lectured two groups of 70 students twice per week in two-hour
+              sessions.
+            </p>
           </div>
         </li>
-        {/*  */}
         <li>
-          <h3>2022: Universidad Nacional del Litoral</h3>
+          <h3>
+            Educational Outreach:{' '}
+            <span style={style}>Mar. 2017 - Dec. 2018</span>
+          </h3>
+
           <div className={styles.indentedSection}>
-            <p>Tutor for "Cálculo 1", 2° Semester.</p>
-            <p>Tutor for "Matemática Discreta", 2° Semester.</p>
+            <p>
+              Collaborating with the Universidad Nacional del Litoral, providing
+              one-to-one tuition in a variety of STEM subjects to less affluent
+              members of the community.
+            </p>
           </div>
         </li>
       </ul>
